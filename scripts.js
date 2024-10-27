91d446f19fc019fe1c94bddff1b82aa689001c73
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-10-28 00:14:41 +1100

    script to async submit forms then reset when successful

--- scripts.js
+++ scripts.js
@@ -1,1 +1,44 @@
-alert('hi');
+const forms = Array.from(document.querySelectorAll('form'));
+
+forms.forEach(form => {
+
+  const { action, method, enctype } = form;
+
+  form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    let headers = {};
+    let body;
+
+    switch (enctype) {
+      case 'multipart/form-data':
+        body = formData;
+        break;
+      case 'application/x-www-form-urlencoded':
+        headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        body = new URLSearchParams(formData);
+        break;
+      default:
+        console.log(`What's a ${ enctype }?`);
+        return false;
+    }
+
+    try {
+      const result = await fetch(action, {
+        method,
+        headers,
+        body,
+      });
+
+      const data = await result.json();
+      console.log(data);
+
+      form.reset();
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  });
+
+});
