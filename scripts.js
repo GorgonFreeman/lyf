584f19c8beae49ccaa8b5174e022ce7fc971d700
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-10-30 12:35:15 +1100

    styles and file dropzone initial

--- scripts.js
+++ scripts.js
@@ -42,3 +42,97 @@ forms.forEach(form => {
   });
 
 });
+
+const addEventListeners = (el, eventsArray, handler) => {
+  eventsArray.forEach(eventName => {
+    el.addEventListener(eventName, handler);
+  });
+};
+
+class FileDropzone extends HTMLElement {
+  constructor() {
+    super();
+    
+    this.files = [];
+    
+    // Inject HTML
+    this.render();
+    
+    // Set up refs
+    this.dropzone = this;
+    this.input = this.querySelector('input[type="file"]');
+    this.display = this.querySelector('.file_dropzone_display');
+    this.clearButton = this.querySelector('.file_dropzone_clear_button');
+    
+    console.log(this.input);
+    console.log(this.input.files);
+    
+    this.updateDisplay();
+
+    // Set up event listeners
+    this.listen();
+  }
+  
+  render() {    
+    this.innerHTML = `
+      <input type="file" multiple>
+      <span class="file_dropzone_display"></span>
+      <button class="file_dropzone_clear_button ${ this.files.length ? '_show' : '' }">X</button>
+    `;
+  }
+  
+  updateDisplay() {
+    const { display, input, clearButton } = this;
+    const { files } = input;
+    console.log('files', files);
+     
+    let message;
+    switch (files.length) {
+      case 0: 
+        message = 'Choose files'
+        break;
+      case 1: 
+        const filename = input.value.split('\\').pop();
+        message = filename;
+        break;
+      default:
+        message = `${ files.length } files`;
+    }
+    
+    console.log('message', message);
+    display.innerHTML = message;
+    
+    clearButton.classList.toggle('_show', files.length);
+  }
+  
+  listen() {
+    
+    const { dropzone, input, clearButton } = this;
+    
+    console.log(dropzone);
+    
+    addEventListeners(dropzone, [
+      'dragenter', 
+      'focus', 
+      'click',
+    ], e => dropzone.classList.add('_active'));
+    
+    addEventListeners(dropzone, [
+      'dragleave', 
+      'blur', 
+      'drop',
+    ], e => dropzone.classList.remove('_active'));
+    
+    input.addEventListener('change', e => {
+      this.files = input.files;
+      this.updateDisplay();
+    });
+    
+    clearButton.addEventListener('click', e => {
+      input.value = null;
+      input.dispatchEvent(new Event('change'));
+    });
+  }
+}
+
+customElements.define('file-dropzone', FileDropzone);
